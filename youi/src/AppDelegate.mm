39ec5ed300281d2124a29dccc480aeb7a7f14637
Julio <jtfrancia@gmail.com>
2020-08-07 12:56:03 -0500

    Update AppDelegate.mm

--- youi/src/AppDelegate.mm
+++ youi/src/AppDelegate.mm
@@ -10,8 +10,8 @@
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary<UIApplicationLaunchOptionsKey,id> *)launchOptions {
-[NewRelic enableFeatures:NRFeatureFlag_NetworkRequestEvents | NRFeatureFlag_NSURLSessionInstrumentation | NRFeatureFlag_RequestErrorEvents | NRFeatureFlag_HttpResponseBodyCapture];
-    [NewRelicAgent startWithApplicationToken:@"AA216ffa362de5f23adaec2ab8c09d1db36102ea91"];
+    return [super application:application didFinishLaunchingWithOptions:launchOptions];
+    return true;
     }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application{
